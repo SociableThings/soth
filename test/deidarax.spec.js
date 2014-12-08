159b320da8dc22b86b797b46c1168b42d1ae5299
Hideyuki Takei <takehide22@gmail.com>
2014-12-09 00:03:26 +0900

    change expect.js to expect

--- test/deidarax.spec.js
+++ test/deidarax.spec.js
@@ -1,10 +1,10 @@
-var expect = require('expect.js');
+var expect = require('expect');
 var deidarax = require('../deidarax.js');
 
 describe('add', function(){
   it('1 + 1 = 2', function(){
     var result = deidarax.add(1, 1);
-    expect(result).to.be(2);
+    expect(result).toBe(2);
   });
 });
 
